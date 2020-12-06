0c6abfd5c909711a784e89c3b0548f45ff979c41
Lukas Bloznelis <lukas.bloznelis@gmail.com>
2020-12-06 11:57:38 +0200

    Removed util import

--- src/main/scala/io/bloznelis/aoc2020/Day6.scala
+++ src/main/scala/io/bloznelis/aoc2020/Day6.scala
@@ -1,9 +1,7 @@
 package io.bloznelis.aoc2020
 
-import io.bloznelis.aoc2020.util.{readBatches, readFileAsString}
-
 object Day6 {
-  val batches = readBatches("data/input/day6.txt")
+  val batches = util.readBatches("data/input/day6.txt")
   val first = batches.map(_.replace("\n","").distinct.size).sum
   val second = batches.map(batch => batch.replace("\n","").mkString("").toCharArray.groupMapReduce(identity)(_ => 1)(_ + _).filter((_, amount) => amount == batch.split("\n").size).size).sum
 }
